6bd9ea521c3e0ff0e8ae8e0d91e298b41bf7ac57
Vanessa Emerick <vavarooks123@gmail.com>
2022-10-12 08:20:00 -0700

    Working on Creation Form

--- flask_app/controllers/projects.py
+++ flask_app/controllers/projects.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template, redirect, request, session, flash
 from flask_bcrypt import Bcrypt
-# from models.production import Product
+from models.production import Product
 
 from flask_app import app
 bcrypt = Bcrypt(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    all_projects = Product.get_all();
+    return render_template("index.html", all_projects = all_projects)
+
+@app.route("/create", methods = "POST")
+def create():
+    data = {
+        "name" : request.form["name"],
+        "name" : request.form["name"],
+        "name" : request.form["name"]
+    }
+    Product.save(data)
+    return redirect("/submit")
 
 @app.route("/submit")
 def submit():
-    return redirect("/")
+    all_projects = Product.get_all();
+    return render_template("form.html", all_projects = all_projects)
